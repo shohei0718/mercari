e9cf3e0f6aafe0b56931cd467d01da2dc527b3ea
keishoji <keishoji@kei.local>
2018-12-03 19:38:25 +0900

    コメントアウト削除　routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items
   resources :users, only: :index
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
