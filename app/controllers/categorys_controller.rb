0fe4c2fe93ef77e75a508718cff389ab4a696a02
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-04 17:39:45 +0900

    pagenation(kaminari)編集

--- app/controllers/categorys_controller.rb
+++ app/controllers/categorys_controller.rb
@@ -1,6 +1,6 @@
 class CategorysController < ApplicationController
   def index
-    @items = Item.includes(:item_images).page(params[:page]).per(1).order("created_at DESC")
+    @items = Item.includes(:item_images).page(params[:page]).per(5).order("created_at DESC")
     @item_images =ItemImage.all
   end
 end
