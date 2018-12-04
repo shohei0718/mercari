d9d98ceabc537ba6fb4612a65bf71c2a3cc992d6
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-04 18:43:37 +0900

    modify
    仮置きで・・・

---
+++ app/controllers/payment_informations_controller.rb
@@ -0,0 +1,19 @@
+class PaymentInformationsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @payment_information = PaymentInformation.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :items
+  resources :payment_informations, only: [:index, :new, :create]
   resources :users, only: :index
 end
