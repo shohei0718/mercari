21b5036a10818af725a08f21dcbb6fa57a5178fc
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-04 10:28:57 +0900

    ページネーション作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.includes(:item_images).all
+    @items = Item.includes(:item_images).page(params[:page]).per(5).order("created_at DESC")
   end
 end
