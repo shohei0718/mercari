91b805f9114b458c9b1bdd555290f3a51a3c49b8
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-05 12:45:24 +0900

    Item.rbのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :name, :user_id, :price, :text, presence: true
+  validates :name, :price, :text, presence: true
 
   has_many   :item_images
   has_many   :communications
