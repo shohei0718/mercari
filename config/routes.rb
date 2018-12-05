5ced123f476a9338b95c599f8c2f9936445a79f6
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-05 12:41:34 +0900

    devise.merge
    石川くんのユーザーログイン機能の実装をマージしたときに出てきたブランチとの差分をコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root 'items#index'
   resources :items
   resources :payment_informations, only: [:index, :new, :create]
   resources :users, only: :index
   resources :categorys
+
 end
