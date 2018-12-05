c6aa69a1a2055feed447f97c887f693dcbf63596
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-05 12:38:32 +0900

    commit（mergeのupdateのため）

--- app/controllers/payment_informations_controller.rb
+++ app/controllers/payment_informations_controller.rb
@@ -5,15 +5,30 @@ class PaymentInformationsController < ApplicationController
 
   def new
     @payment_information = PaymentInformation.new
+
   end
 
   def create
+    # binding.pry
+    @payment_information = PaymentInformation.create(payment_information_params)
+
+    redirect_to :back
+
+    # あとで入れる,(card_number: payment_information_params[:card_number], valid_year: payment_information_params[:valid_year],valid_month: payment_information_params[:valid_month],cvc: payment_information_params[:cvc],user_id: current_user.id)
+
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def payment_information_params
+    params.permit(:card_number, :valid_year, :valid_month, :cvc)
+
   end
 
 end
