b65562563a9f8408732973ed09e8636723ca5746
keishoji <keishoji@kei.local>
2018-12-07 12:01:02 +0900

    商品出品コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    if user_signed_in?
+      @item = current_user.items.new
+      @item.item_images.build
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
+    if user_signed_in?
+      @item = current_user.items.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new, item_images: @item.item_images.build
+      end
+    end
   end
 
   def edit
@@ -19,5 +32,16 @@ class ItemsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :price, :description, :first_category_id,
+      :second_category_id, :third_category_id, :brand_id,
+      :size_id, :condition_id, :delivery_charge_id, :prefecture_id,
+      :delivery_date_id, :order_status_id, :delivery_way_id, item_images_attributes:[:id, :image])
+  end
+
 end
 
