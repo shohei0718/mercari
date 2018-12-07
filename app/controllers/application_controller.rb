e917e0f8dc477798feaaa09103568a9d71343d3e
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-07 12:17:06 +0900

    バリデーションのバグ修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:first_name_kana,:last_name,:last_name_kana,:prefecture_id,:city,:address,:building,:zip_code,:telephone,:birth_year,:birth_month,:birth_day])
+  end
   def production?
     Rails.env.production?
   end
