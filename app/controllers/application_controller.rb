7ff7f497bd4d727cefa78b12289f6eee2f4db4ec
taiga39 <taiga@taiga.local>
2018-12-07 16:05:24 +0900

    アプリケーションコントローラの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name,:first_name_kana,:last_name,:last_name_kana,:prefecture_id,:city,:address,:building,:zip_code,:telephone,:birth_year,:birth_month,:birth_day])
+  end
+
   def production?
     Rails.env.production?
   end
