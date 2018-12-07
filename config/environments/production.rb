bd6688840a7218b0a0bd02a375bfa22537dfe980
Tomoharu Shimbara <shimba005@gmail.com>
2018-12-07 17:13:14 +0900

    Logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.logger = Logger.new("log/test.log")
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
