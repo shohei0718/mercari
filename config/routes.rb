a5f637c6cc3fe6fce30fefdd60fd103dc15cf2a1
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-10 15:10:53 +0900

    ユーザー本人確認ページとユーザープロフィール編集ページのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'items#index'
-  resources :users, only: :show
+  resources :users
   resources :items
   match 'secondcategory', to: 'items#secondcategory', via: [:get, :post]
   match 'thirdcategory', to: 'items#thirdcategory', via: [:get, :post]
   resources :categorys
   get "logout" => 'users#logout'
   get "order-confirm" => 'items#order-confirm'
-  get "edit-profile" => 'users#edit-profile'
+  get "users/edit_profile/:id" => 'users#edit_profile', as: 'users_edit_profile'
+  get "users/user_info/:id" => 'users#user_info', as: 'users_user_info'
+  # patch "users/edit_profile/:id" => 'users#update'
+  # patch "users/user_info/:id" => 'users#update'
 end
