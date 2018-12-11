2cd61a204606ed6b946deb85f42bbfaf8138695c
ihrsyuu <iharasyuutarou@iharahiiragitarounoMacBook-puro.local>
2018-12-11 12:19:18 +0900

    微修正

--- spec/factories/item_images.rb
+++ spec/factories/item_images.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
 
   factory :item_image do
-    image             { Faker::Avatar.image }
+    image             { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/spec.jpg')) }
+    created_at          { Faker::Time.between(2.days.ago, Time.now, :all) }
+    updated_at          { Faker::Time.between(2.days.ago, Time.now, :all) }
     association :item,  factory: :item
   end
 
