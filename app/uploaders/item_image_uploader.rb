8fe9bf4270397a73d12c78d6336510c9ed81a890
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-11 21:25:46 +0900

    commit（本番環境でのみ）

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -6,7 +6,13 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :file
   # storage :fog
-  storage :fog
+  # storage :fog
+  case Rails.env
+  when 'production'
+    storage :fog
+  else
+    storage :file
+  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
