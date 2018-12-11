e41fbe09960d5020e458eadefd3627a8e42dc9ce
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-11 16:58:14 +0900

    commit（単体テスト/pending）

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe OrdersController, type: :controller do
+
+  describe "GET #show" do
+
+    it "returns a 200 response" do
+
+    end
+
+    it "assigns @order" do
+
+    end
+
+    it "renders the :show template" do
+
+    end
+  end
+
+end
