4b9a0fc4081c5da3a0be5be8f06d793e89aa5618
shohei0718 <Shohei@MacBook-Pro-7.local>
2018-12-19 14:19:50 +0900

    modify（マイグレーションファイルの修正）
    マイグレーションファイルの修正

--- db/migrate/20181130071755_create_third_categories.rb
+++ db/migrate/20181130071755_create_third_categories.rb
@@ -1,7 +1,7 @@
 class CreateThirdCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :third_categories do |t|
-      t.string     :third_category,      null:false
+      t.string     :third_category
 
       # t.references :second_category,     foreign_key: true
 
